f3e7d5b2bf7d69521d9d25bb95fefb422365dac3
Reece-Ausmus <112425322+Reece-Ausmus@users.noreply.github.com>
2023-12-18 17:15:52 -0500

    added method to build frequency table

--- huffman_compression.py
+++ huffman_compression.py
@@ -11,3 +11,13 @@ class TreeNode:
         """Performs less than comparison based on frequency"""
         return (self.frequency < other.frequency)
     
+def buildFrequencyTable(text):
+    """This method creates a frequency table for the text that it receives"""
+    frequency_table = {}
+    for char in text:
+        if char in frequency_table:
+            frequency_table[char] += 1
+        else:
+            frequency_table[char] = 1
+    return frequency_table
+
